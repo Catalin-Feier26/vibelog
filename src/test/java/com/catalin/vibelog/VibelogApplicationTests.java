a3a58988d8ec56115986b4c7b618fe192c52e7a3
Catalin <feiercatalin123@icloud.com>
2025-03-21 09:58:51 +0200

    Initial commit: frontend & backend setup

---
+++ src/test/java/com/catalin/vibelog/VibelogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catalin.vibelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibelogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
