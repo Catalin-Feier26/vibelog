c0f2822af7f87a062b34424aad7b487994ff6c63
Catalin <feiercatalin123@icloud.com>
2025-03-24 19:12:29 +0200

    Media service implementation

--- src/main/java/com/catalin/vibelog/service/MediaService.java
+++ src/main/java/com/catalin/vibelog/service/MediaService.java
@@ -1,4 +1,25 @@
 package com.catalin.vibelog.service;
 
+import com.catalin.vibelog.model.Media;
+import com.catalin.vibelog.repository.MediaRepository;
+import com.catalin.vibelog.model.MediaType;
+
+import java.util.List;
+
 public interface MediaService {
+    Media uploadMedia(Media media);
+
+    Media getMediaById(Long id);
+    List<Media> getAllMedia();
+
+    List<Media> getMediaByType(MediaType type);
+
+    List<Media> getMediaByFormat(String format);
+
+    Media getMediaByUrl(String url);
+
+    void deleteMedia(Long id);
+
+    List<Media> getMediaForPost(Long postId);
+
 }
